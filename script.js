6c8c3176db3045b6cac380d1e7a0925e9edc8ec7
Панов Даниил <kartoshkavkusnay@gmail.com>
2024-08-01 15:50:41 +0300

    add again

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.getElementById('toggle-light').addEventListener('click', function() {
+    const body = document.body;
+    const lightbulbIcon = document.getElementById('lightbulb-icon');
+
+    if (body.classList.contains('light-on')) {
+        body.classList.remove('light-on');
+        lightbulbIcon.src = '/photo/light.png';
+    } else {
+        body.classList.add('light-on');
+        lightbulbIcon.src = '/photo/lightoff.png';
+    }
+});
