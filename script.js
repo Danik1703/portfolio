f327cfca603db8b9c36f4d7b26532765a7f72c00
Панов Даниил <kartoshkavkusnay@gmail.com>
2024-08-02 17:47:03 +0300

    add

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-
-document.getElementById('toggle-light').addEventListener('click', function() {
+document.getElementById('toggle-light').addEventListener('click', function () {
     const body = document.body;
     const lightbulbIcon = document.getElementById('lightbulb-icon');
 
     if (body.classList.contains('light-on')) {
         body.classList.remove('light-on');
-        lightbulbIcon.src = '/photo/light.png';
+        lightbulbIcon.src = 'photo/light.png';
     } else {
         body.classList.add('light-on');
-        lightbulbIcon.src = '/photo/lightoff.png';
+        lightbulbIcon.src = 'photo/lightoff.png';
     }
 });
+
+
